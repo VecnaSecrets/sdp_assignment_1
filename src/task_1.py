00d47d8fee6ded6cb4e306d518fe614a161ba575
Вячеслав Блинов <gravitycat35@gmail.com>
2022-09-20 17:29:46 +0300

    Code Review

--- src/task_1.py
+++ src/task_1.py
@@ -3,11 +3,17 @@ from time import time
 
 def decorator_1(func):
     def wrap_func(*args, **kwargs):
+        wrap_func.counter += 1
+
         t1 = time()
         result = func(*args, **kwargs)
         t2 = time()
-        wrap_func.counter += 1
-        print(f'Function {func.__name__!r} executed in {(t2 - t1):.4f}s Number of calls: ', wrap_func.counter)
+
+        print(
+            f"Function {func.__name__!r} executed in {(t2 - t1):.4f}s.\nNumber of calls: {wrap_func.counter}."
+        )
         return result
+
     wrap_func.counter = 0
+
     return wrap_func
--- src/task_3.py
+++ src/task_3.py
@@ -1,8 +1,7 @@
-import inspect
-from time import time
-from inspect import *
 import io
 from contextlib import redirect_stdout
+from inspect import *
+from time import time
 
 
 ranks = {}
@@ -15,45 +14,50 @@ class Decorator3:
     def __init__(self, func):
         self.func = func
 
-    def get_output(self, args, kwargs):
-        def inner(output):
-            str = ''
+    def get_output(self, args, kwds):
+        def tabulation(output):
+            str = ""
             for line in output:
-                str += '\t' + line + '\n'
+                str += "\t" + line + "\n"
             return str
 
         with io.StringIO() as buf, redirect_stdout(buf):
-            self.func(args, kwargs)
-            return inner(buf.getvalue().splitlines())
+            self.func(args, kwds)
+            return tabulation(buf.getvalue().splitlines())
 
-    def print_info(self, args, kwargs):
-        print(f'Name:    {self.func.__name__}')
-        print(f'Type:    {type(self.func)}')
-        print(f'Sign:    {signature(self.func)}')
-        print(f'Args:    positional {args}\n\t\t key-worded {kwargs}')
-        print(f'Doc:     {self.func.__doc__}')
-        print(f'Source:\n{inspect.getsource(self.func)}')
-        print(f'Output:\n{self.get_output(args, kwargs)}')
+    def print_info(self, args, kwds):
+        print(f"Name:    {self.func.__name__}")
+        print(f"Type:    {type(self.func)}")
+        print(f"Sign:    {signature(self.func)}")
+        print(f"Args:    positional {args}\n\t\t key-worded {kwds}")
+        print(f"Doc:     {self.func.__doc__}")
+        print(f"Source:\n{getsource(self.func)}")
+        print(f"Output:\n{self.get_output(args, kwds)}")
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args, **kwds):
         global ranks
-        t1 = time()
-        result = self.func(*args, **kwargs)
-        t2 = time()
         self.counter += 1
+
+        t1 = time()
+        result = self.func(*args, **kwds)
+        t2 = time()
         res = t2 - t1
+
         with io.StringIO() as buf, redirect_stdout(buf):
-            with open('output_task_3.txt', 'a') as output_file:
-                print(f'Function {self.func.__name__!r} executed in {res:.4f}s Number of calls: ', self.counter)
-                self.print_info(args, kwargs)
+            with open("output_task_3.txt", "a") as output_file:
+                print(
+                    f"Function {self.func.__name__!r} executed in {res:.4f}s.\nNumber of calls: {self.counter}."
+                )
+                self.print_info(args, kwds)
                 for line in buf.getvalue().splitlines():
-                    output_file.write(line + '\n')
+                    output_file.write(line + "\n")
+
         ranks[self.func.__name__] = [res]
+
         return result
 
     @staticmethod
     def print_rank():
-        print('PROGRAM | RANK | TIME ELAPSED')
+        print("PROGRAM | RANK | TIME ELAPSED")
         for index, (k, v) in enumerate(sorted(ranks.items(), key=lambda item: item[1])):
-            print(f'{k}\t\t{index+1}\t\t{v[0]:.9f}s')
-
+            print(f"{k}\t\t{index+1}\t\t{v[0]:.9f}s")
